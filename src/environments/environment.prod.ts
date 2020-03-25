9cc7d84c528174c1e98b52ae08fcbac03843b5c1
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-03-25 09:15:34 +0200

    changed api_url for the news

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_AUTH_URL: 'https://footballfun-74762.firebaseio.com/users.json',
-  API_NEWS_URL: 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-24&sortBy=publishedAt&apiKey=afaa592a36b84c3baf567701c20ce13d'
+  API_NEWS_URL: 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-25&sortBy=publishedAt&apiKey=afaa592a36b84c3baf567701c20ce13d'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   API_AUTH_URL: 'https://footballfun-74762.firebaseio.com/users.json',
-  API_NEWS_URL: 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-24&sortBy=publishedAt&apiKey=afaa592a36b84c3baf567701c20ce13d'
+  API_NEWS_URL: 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-25&sortBy=publishedAt&apiKey=afaa592a36b84c3baf567701c20ce13d'
 };
 
 /*
